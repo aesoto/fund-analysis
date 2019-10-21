7e48b6fbc7717acc8baede63a4c53d899f40ec53
Andres Soto <andresegidiosoto@gmail.com>
2019-10-20 21:57:23 -0500

    Add files via upload

---
+++ main.R
@@ -0,0 +1,11 @@
+# Top Down Attribution - Funds
+# 7 August 2019
+
+attributionLibraries <- c('data.table', 'dplyr', 'tidyr', 'lubridate', 'purrr', 'bizdays', 'rowr', 'here', 'writexl')
+lapply(attributionLibraries, require, character.only=TRUE)
+
+source(here('functions', 'mainFuncs.R'))
+
+if (sys.nframe() == 0) {
+  main()
+}
