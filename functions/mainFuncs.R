dec386c45fed1fc40f67b9d16f329d38655a67e2
Andres Soto <andresegidiosoto@gmail.com>
2019-11-08 18:42:30 -0600

    Update mainFuncs.R

--- functions/mainFuncs.R
+++ functions/mainFuncs.R
@@ -1,15 +1,17 @@
 source(here('functions', 'inputFuncs.R'))
 source(here('functions', 'getFuncs.R'))
 source(here('functions', 'cleanFuncs.R'))
-source(here('functions', 'prepFuncs v2.R'))
+source(here('functions', 'prepFuncs.R'))
 source(here('functions', 'attribFuncs.R'))
 source(here('functions', 'saveFuncs.R'))
+source(here('functions', 'bottomUpFuncs.R'))
 
 main <- function() {
-  input <- getInputs()
-  rawData <- getData(input)
-  cleanedData <- cleanData(rawData)
+  inputs <- getInputs()
+  rawData <- getData(inputs)
+  cleanedData <- cleanData(rawData, inputs)
   preparedData <- prepData(cleanedData)
-  output <- getAttribution(preparedData, isGeometric)
-  saveData(output, date)
+  outputTopDown <- getAttribution(preparedData, inputs)
+  outputBottomUp <- getBottomsUp(preparedData, inputs)
+  saveData(outputTopDown, outputBottomUp, inputs$date)
 }
